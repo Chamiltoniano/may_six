161a896d2e0c795e4a87c6c91ed6c7270e071b6b
Chamiltoniano <vmmartinez@pol.una.py>
2025-05-13 20:50:55 -0300

    new changes
    new things added

--- app.py
+++ app.py
@@ -6,17 +6,29 @@ def home():
     return '''
     <html>
     <head>
-        <link rel="stylesheet" href="/static/style.css">
+    <title>Acerca de</title>
+    <link rel="icon" href="/static/musicaon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="/static/style.css">
     </head>
     <body>
-        <div class="contenedor">
-            <h1>¡Hola desde Render2!</h1>
-            <img src="/static/spchamy1.png" alt="Imagen centrada">
-        </div>
+    <div class="contenedor">
+    <h1>¡Hola desde Render2!</h1>
+    <img src="/static/spchamy1.png" alt="Imagen centrada">
+    </div>
+
+    <script>
+    const hora = new Date().getHours();
+    if (hora >= 18 || hora < 6) {
+    document.body.style.backgroundColor = "#1c1c1c";
+    document.body.style.color = "#fff";
+    }
+    </script>
     </body>
+
     </html>
+
     '''
 
-import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
